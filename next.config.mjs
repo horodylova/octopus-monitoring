76b820c66095c9b319793b31a33a59737e216516
Svitlana Horodylova <horodylova.sv@gmail.com>
2026-02-26 17:15:25 +0000

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  swcMinify: false,
   images: {
     domains: [],
     formats: ['image/avif', 'image/webp'],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+    return config;
   },
   async headers() {
     return [
